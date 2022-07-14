c6d7ab16d4758ad1f74d8d0fa717d9641f96d52c
Alperen Onal <alperenonal9922@gmail.com>
2022-07-14 13:39:32 +0300

    Movement Animation
    + Creating Animation Controller
    + Creating Movement Blend Tree
    + Creating AnimationManager Script

---
+++ Third Person Fight Game/Assets/Scripts/AnimatorManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    int horizontal;
+    int vertical;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal");
+        vertical = Animator.StringToHash("Vertical");
+    }
+
+    // this func. should be called from input manager
+    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
+    {
+        float snappedHorizontal;
+        float snappedVertical;
+
+        #region Snapped Horizontal
+        if (horizontalMovement > 0 && horizontalMovement < .55f)
+        {
+            snappedHorizontal = .5f;
+        }
+        else if (horizontalMovement > .55f)
+        {
+            snappedHorizontal = 1;
+        }
+        else if (horizontalMovement < 0 && horizontalMovement > -.55f)
+        {
+            snappedHorizontal = -.5f;
+        }
+        else if (horizontalMovement < -.55f )
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+        #region Snapped Vertical
+        if (verticalMovement > 0 && verticalMovement < .55f)
+        {
+            snappedVertical = .5f;
+        }
+        else if (verticalMovement > .55f)
+        {
+            snappedVertical = 1;
+        }
+        else if (verticalMovement < 0 && verticalMovement > -.55f)
+        {
+            snappedVertical = -.5f;
+        }
+        else if (verticalMovement < -.55f)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+        animator.SetFloat(horizontal, snappedHorizontal, .1f, Time.deltaTime);
+        animator.SetFloat(vertical, snappedVertical, .1f, Time.deltaTime);
+    }
+}
